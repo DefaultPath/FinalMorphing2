72d9ff3bce5acb4189a3ef0f0f2c8da389459d02
Lux998 <lux998@yahoo.it>
2015-05-21 22:36:03 +0200

    Update

--- src/finalmorphing2/MotoreTriangolazione.java
+++ src/finalmorphing2/MotoreTriangolazione.java
@@ -20,38 +20,37 @@ public class MotoreTriangolazione
 	coppie.add(new CoppiaTriangoli(t2Editor1,t2Editor2));
 	index=0;
     }
-    public Triangolo trovaTriangoloContenitoreDi(Punto p ,int chisono)//Chisono = 0 sorgete chisono=1 destinatario
+    public CoppiaTriangoli trovaTriangoliContenitoriDi(Punto p )//Chisono = 0 sorgete chisono=1 destinatario
     {
-	Triangolo t = null;
-	
+	CoppiaTriangoli t;
+	Triangolo a = null,b = null; 
 	for(int i=0;i<coppie.size();i++)
 	{
-	    if(chisono==0)
-	    {
+	    
 		if(coppie.get(i).sorgente.belongTriangle(p))
 		{
-		    t= coppie.get(i).sorgente;
+		    a= coppie.get(i).sorgente;
 		    index=i;		    
 		}
-	    }
-	    else if(chisono==1)
-	    {
-		if(coppie.get(i).destinazione.belongTriangle(p))
+	    	    {
+		if(coppie.get(i).destinazione.belongTriangle(p.myTwin))
 		{
-		    t= coppie.get(i).destinazione;
+		    b= coppie.get(i).destinazione;
 		    index=i;
 		}
 	    }
 	}
+	t=new CoppiaTriangoli(a,b);
 	    return t;
     }
 	public void AggiungiPunto(Punto p,int chisono)
 	{
-	    Triangolo contenitore = trovaTriangoloContenitoreDi(p,chisono);
+	    CoppiaTriangoli contenitore = trovaTriangoliContenitoriDi(p);
 	    Punto vertice1,vertice2,vertice3;
-	    vertice1=contenitore.v1;
-	    vertice2=contenitore.v2;
-	    vertice3=contenitore.v3;
+	    coppie.add(new CoppiaTriangoli(new Triangolo(contenitore.sorgente.v1,contenitore.sorgente.v2,p),new Triangolo(contenitore.destinazione.v1,contenitore.destinazione.v2,p)));
+	    coppie.add(new CoppiaTriangoli(new Triangolo(contenitore.sorgente.v2,contenitore.sorgente.v3,p),new Triangolo(contenitore.destinazione.v2,contenitore.destinazione.v3,p)));
+	    coppie.add(new CoppiaTriangoli(new Triangolo(contenitore.sorgente.v1,contenitore.sorgente.v3,p),new Triangolo(contenitore.destinazione.v1,contenitore.destinazione.v3,p)));
+	    coppie .remove(contenitore);
 	    
 	    //coppie.remove(coppie.indexOf(contenitore));
         //rimuovo la coppia che contiene questo triangolo
@@ -59,4 +58,5 @@ public class MotoreTriangolazione
 	//coppie.add(t);
 	}
     }
+
 
