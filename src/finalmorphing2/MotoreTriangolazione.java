afdb0f988ef43776b24382a013b0e6f051d79d9d
Lux998 <lux998@yahoo.it>
2015-05-21 18:23:35 +0200

    Progress
    Triangolazione dei contenitori

--- src/finalmorphing2/MotoreTriangolazione.java
+++ src/finalmorphing2/MotoreTriangolazione.java
@@ -12,18 +12,41 @@ import java.util.ArrayList;
  */
 public class MotoreTriangolazione 
 {
-     ArrayList<CoppiaTriangoli> couple = new ArrayList();
+    ArrayList<CoppiaTriangoli> coppie = new ArrayList(); 
     public MotoreTriangolazione(Triangolo t1Editor1,Triangolo t1Editor2,Triangolo t2Editor1,Triangolo t2Editor2)            
     {
-        couple.add(new CoppiaTriangoli(t1Editor1,t1Editor2));
-	couple.add(new CoppiaTriangoli(t2Editor1,t2Editor2));
+        coppie.add(new CoppiaTriangoli(t1Editor1,t1Editor2));
+	coppie.add(new CoppiaTriangoli(t2Editor1,t2Editor2));
     }
-    public void AggiungiCoppia(CoppiaTriangoli t)
+    public Triangolo trovaTriangoloContenitoreDi(Punto p ,int chisono)//Chisono = 0 sorgete chisono=1 destinatario
     {
-	couple.add(t);
+	Triangolo t = null;
+	for(int i=0;i<coppie.size();i++)
+	{
+	    if(chisono==0)
+	    {
+		if(coppie.get(i).sorgente.belongTriangle(p))
+		{
+		    t= coppie.get(i).sorgente;	    
+		}
+	    }
+	    else if(chisono==1)
+	    {
+		if(coppie.get(i).destinazione.belongTriangle(p))
+		{
+		    t= coppie.get(i).destinazione;		    
+		}
+	    }
+	}
+	    return t;
     }
-    /*public Triangolo CreaTriangolo(Punto p)
-    {
-	for(int i=0;i<)
-    }*/
+	public void AggiungiPunto(Punto p,int chisono)
+	{
+	    Triangolo contenitore = trovaTriangoloContenitoreDi(p,chisono);
+	    //coppie.remove(coppie.indexOf(contenitore));
+        //rimuovo la coppia che contiene questo triangolo
+        //trovo i tre triangoli in entrambi gli editor
+	//coppie.add(t);
+	}
+    }
 }
