adeb9d00f1aa8321527fff3bc39f168d690b451f
Lux998 <lux998@yahoo.it>
2015-06-15 14:13:01 +0200

    DS
    D

--- src/finalmorphing2/Triangolo.java
+++ src/finalmorphing2/Triangolo.java
@@ -43,15 +43,20 @@ public class Triangolo
     }
         public boolean belongTriangle(Pixel p,int x,int y)
     {
-        double s,t,r;
+        /*double s,t,r;
         s = 1/(2*this.getArea())*(v1.getY()*v3.getX() - v1.getX()*v3.getY() + (v3.getY() - v1.getY())*x + (v1.getX() - v3.getX())*y);
         t = 1/(2*this.getArea())*(v1.getX()*v2.getX() - v1.getY()*v2.getX() + (v1.getY() - v2.getY())*x + (v2.getX() - v1.getX())*y);
         r = 1-s-t;
         if(s>0 && t>0 && r>0)
             return true;
         else 
-            return false;
-    }
+            return false;*/
+	boolean b1,b2,b3;
+	b1 = sign(new Punto(x, y, false, null), v1, v2) < 0.0d;
+	b2 = sign(new Punto(x, y, false, null), v2, v3) < 0.0d;
+	b3 = sign(new Punto(x, y, false, null), v3, v1) < 0.0d;
+	return ((b1==b2) && (b2==b3));
+    }	
     public double getPerimetro()
     {
         return latoAB+latoAC+latoBC;
@@ -59,5 +64,9 @@ public class Triangolo
     public double getArea()
     {
        return Math.sqrt(this.getPerimetro()/2 * (this.getPerimetro()/2 - latoAB) * (this.getPerimetro()/2 - latoBC) * (this.getPerimetro()/2 - latoAC));
+    }
+    private double sign(Punto p1,Punto p2,Punto p3)
+    {
+	return (p1.getX() - p3.getX()) * (p2.getY() - p3.getY()) - (p2.getX() - p3.getX()) * (p1.getY() - p3.getY());
     }
 }
